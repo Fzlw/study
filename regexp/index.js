9dd7704f1e812b0f24334f29acb0d374275cc64c
liwei <liwei@madingyu.com>
2018-08-29 23:04:16 +0800

    home update

--- regexp/index.js
+++ regexp/index.js
@@ -2,5 +2,12 @@ let str = "<h1>Chapter 1 - 介绍正则表达式</h1>";
 
 let reg = /<.*>/;
 reg = /<.*?>/;
-console.log(str.match(reg));
+// console.log(str.match(reg));
+
+// 选择数字
+
+let rNum = /\d+/g; 
+let str1 = 'banktest978675643265342452453453';
+
+console.log(str1.match(/\d+/g));
   
