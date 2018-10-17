f10720844c683aa9372108303ca0750218e9eefa
liwei <liwei@madingyu.com>
2018-10-17 23:56:24 +0800

    down check

--- game/app/public/src/flappybird/pipe.js
+++ game/app/public/src/flappybird/pipe.js
@@ -1,11 +1,17 @@
+/**
+ * 碰撞模型采用AABB盒模型，理解为小鸟就是一个只会上下动的盒子
+ * 后续优化可以加入角度的计算
+ */
+
 class Pipe {
-    constructor(ctx, R, data, pipeArr) {
+    constructor(ctx, R, data, pipeArr, timmer) {
         this.ctx = ctx;
         this.data = data;
-        this.pipeArr = pipeArr;  // 存储管子实例的数组
+        this.pipeArr = pipeArr; // 存储管子实例的数组
+        this.timmer = timmer; // 定时器指针
         this.img = R["allImg"];
-        this.pipeSpeed = data.landSpeed;   // 管子移动速度，和大地移速一样
-        this.entranceWidth = data.entranceWidth;  // 管子的入口宽度
+        this.pipeSpeed = data.landSpeed; // 管子移动速度，和大地移速一样
+        this.entranceWidth = data.entranceWidth; // 管子的入口宽度
         this.upPipe = {
             position: [112, 646],
             size: [52, 320]
@@ -20,7 +26,8 @@ class Pipe {
     init() {
         const sH = this.upPipe.size[1],
             baseH = -(sH - 100);
-        this.startX = this.data.WIDTH + 288; // 管子的起始X坐标
+        this.birdSize = [34, 24]; // 小鸟的大小
+        this.startX = this.data.WIDTH + 288; // 管子的起始X坐标  + 背景宽
         this.posY_u = baseH + parseInt(Math.random() * 150);
         this.posY_d = sH - Math.abs(this.posY_u) + this.entranceWidth;
         this.create();
@@ -40,13 +47,38 @@ class Pipe {
         this.ctx.drawImage(this.img, sx_d, sy_d, sW, sH, this.startX, this.posY_d, sW, sH);
     }
 
-    update() {
+    /**
+     * 
+     * @param {number} birdY 小鸟盒模型左上角Y坐标
+     */
+    update(birdPosition) {
         this.startX = this.startX - this.pipeSpeed;
         // 如果消失就清除，突变的那一帧会缺失，所以消失一段距离后在删除 * 4
-        if (this.startX <= - 4 * this.upPipe.size[0]) {
+        if (this.startX <= -4 * this.upPipe.size[0]) {
             this.pipeArr.shift();
             return;
         }
+        // 进行碰撞检测
+        this.checkTouch(birdPosition);
         this.create();
+    }
+
+    /**
+     * 碰撞检测
+     * @param {arr} birdPosition 小鸟盒模型左上角XY坐标
+     */
+    checkTouch(birdPosition) {
+        // 管子碰撞检测
+        let pipe_u_y = this.posY_u + this.upPipe.size[1],
+            pipe_u_x = this.startX,
+            pipe_d_y = this.posY_d,
+            [X, Y] = birdPosition;
+        console.log((Y < pipe_d_y && X < pipe_u_x + this.upPipe.size[0] && X > pipe_u_x))
+        console.log(Y, pipe_d_y)
+        if ((Y < pipe_u_y && (X + this.birdSize[0]) >= pipe_u_x) ||
+            (Y < pipe_d_y && X < pipe_u_x + this.upPipe.size[0] && X > pipe_u_x)
+        ) {
+            clearInterval(this.timmer);
+        }
     }
 }
