ad100cad04e0c4d5ddc842768bfa56253e52b034
李伟 <1416017757@qq.com>
2019-07-05 18:57:39 +0800

    websocket todo

--- socket/server/app.js
+++ socket/server/app.js
@@ -1,12 +1,22 @@
 const WebSocket = require('ws');
 const http = require('http');
-const server = http.createServer().listen(6000);
+// const server = http.createServer().listen(3000);
+let count = 0;
 
+const ws = new WebSocket.Server({
+  noServer: true,
+  port: 3000
+});
 
-new WebSocket.Server({
-  host: '127.0.0.1',
-  port: 6000,
-  server
-}, (...args) => {
-  console.log(args)
+ws.on('listening', () => {
+  console.log('启动...')
+})
+ws.on('connection', newConnect => {
+  count ++;
+  console.log('连接成功: 第%d个', count);
+  newConnect.send('Welcoming...')
+
+  newConnect.on('message', data => {
+    console.log(data)
+  })
 })
